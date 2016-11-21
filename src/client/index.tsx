c6196447248b7503598842183ee615fab05759b3
Kyle Brown <picokyle@gmail.com>
2016-11-21 08:20:00 -0700

    Add react-router

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router, Route, hashHistory } from 'react-router'
 import { createStore } from 'redux'
 import App from './components/App'
 import reducer from './reducers'
@@ -8,7 +9,9 @@ import reducer from './reducers'
 render(
   <Provider store={ createStore(reducer) }>
     <div className="container">
-      <App />
+      <Router history={ hashHistory }>
+        <Route path="/" component={ App } />
+      </Router>
     </div>
   </Provider>,
   document.getElementById('root'))
